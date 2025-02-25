5e60f2d6a0c75efbc2e656d4b8bff50525dc82cf
Zach <zachary.thoene@okstate.edu>
2025-02-25 14:53:05 -0600

    Initial Commit

---
+++ Team8GroupProject/Team8GroupProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Team8GroupProjectApp.swift
+//  Team8GroupProject
+//
+//  Created by Thoene, Zachary on 2/25/25.
+//
+
+import SwiftUI
+
+@main
+struct Team8GroupProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
