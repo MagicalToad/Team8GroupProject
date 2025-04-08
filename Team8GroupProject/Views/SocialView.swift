0d583cd8d4e3c43c5424b0f3aeb41868aae21060
Zach <zachary.thoene@okstate.edu>
2025-04-07 20:30:42 -0500

    Added Three New View, Goals, Planner, And Social View, all views default to darkmode

---
+++ Team8GroupProject/Views/SocialView.swift
@@ -0,0 +1,18 @@
+//
+//  SocialView.swift
+//  Team8GroupProject
+//
+//  Created by Thoene, Zachary on 4/7/25.
+//
+
+import SwiftUI
+
+struct SocialView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SocialView()
+}
